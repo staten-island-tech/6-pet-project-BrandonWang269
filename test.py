13b6df526bf19c006e83769cc560d064492a1144
BrandonWang269 <brandonw269@nycstudents.net>
2026-04-24 10:16:52 -0400

    Pet

--- test.py
+++ test.py
@@ -9,7 +9,7 @@
         return numbers
 calculator.add(5,11) """
 
-class Hero:
+""" class Hero:
     def __init__(self, name, money, inventory):
         self.name = name
         self.money = money
@@ -18,6 +18,43 @@ class Hero:
         self.inventory.append(item)
         print(self.inventory)
 
-Jullian = Hero("Julloan", 150, ["Potion"])
+Jullian = Hero("Jullian", 150, ["Potion"])
 Jullian.buy({"title": "Sword", "atk": 34})
 print(Jullian.__dict__)
+
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance
+    
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def show_balance(self):
+        print(f"{self.owner} has ${self.__balance}") """
+
+""" class Hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+
+Cipher = Hero("Cipher", 1000, ["Wand"])
+Cipher.buy({"title": "Hat", "def": 67})
+print(Cipher.__dict__) """
+
+class Pet:
+    def __init__(self, name, happiness):
+        self.name = name
+        self.__happiness = happiness
+    def play(self, increase):
+        self.__happiness += increase
+    def show_status(self):
+        print(f"{self.name} has {self.__happiness} happiness")
+
+Carson = Pet("Carson", 0)
+Carson.play(15)
+Carson.show_status()
